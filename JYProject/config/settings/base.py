3e2e9b9f9ef3f79027470c322ccf6385ce10d5e1
ohjeyong <ojy6042@kaist.ac.kr>
2017-05-15 21:09:21 +0900

    add rest api

--- JYProject/config/settings/base.py
+++ JYProject/config/settings/base.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'JYProject.user',
-    'JYProject.todo'
+    'JYProject.todo',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
