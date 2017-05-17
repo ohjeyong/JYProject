850b24b3426562e703d167941d9a51d1152ee9ac
ohjeyong <ojy6042@kaist.ac.kr>
2017-05-17 17:43:41 +0900

    apply different style and rendering between complete and non-complete

--- front-end/src/components/TodoList.js
+++ front-end/src/components/TodoList.js
@@ -1,34 +1,54 @@
 /**
  * Created by oh on 5/16/17.
  */
-import React from 'react';
+import React, { Component } from 'react';
 import { Icon, Label, Button } from 'semantic-ui-react';
 import TimeAgo from 'timeago-react';
 
-const TodoList = ({ todo }) => {
-    return (
-        <li className="TodoLi">
-            <div className="TodoLeftWrapper">
-                <div className="upper">
-                    <Label color="orange" size="mini">
-                        <Icon name="food" /> 음식
-                    </Label>
-                    <span className="like"><Icon name="like" />{ todo.like }</span>
-                    <div className="TodoContent">
-                        { todo.content }
+class TodoList extends Component {
+    renderRightWrapper = (todo) => {
+        if(todo.is_completed) {
+            return (
+                <div className="TodoRightWrapper">
+                    <span style={{fontSize: '1.2rem', color:'teal'}}>완료</span>
+                    <TimeAgo datetime={ todo.complete_at } locale="ko"/>
+                    <Button style={{marginTop: "10px"}} size="tiny" circular icon="reply" />
+                </div>
+            )
+        }else{
+            return (
+                <div className="TodoRightWrapper">
+                    <Button size="tiny" positive circular icon="checkmark" />
+                    <Button size="tiny" circular icon="trash" />
+                </div>
+            )
+        }
+    };
+    render() {
+        return (
+            <li className={this.props.todo.is_completed ? 'TodoLi complete' : 'TodoLi'}>
+                <div className="TodoLeftWrapper">
+                    <div className="mask"></div>
+                    <div className="upper">
+                        <Label color="orange" size="mini">
+                            <Icon name="food" /> 음식
+                        </Label>
+                        <span className="like"><Icon name="like" />{ this.props.todo.like }</span>
+                        <div className="TodoContent">
+                            { this.props.todo.content }
+                        </div>
+                    </div>
+                    <div className="lower">
+                        <TimeAgo datetime={ this.props.todo.created_at } locale="ko" />
+                        <span style={{float: 'right'}}><Icon name="user" />{ this.props.todo.author_data.name }</span>
                     </div>
                 </div>
-                <div className="lower">
-                    <TimeAgo datetime={ todo.created_at } locale="ko" />
-                    <span style={{float: 'right'}}><Icon name="user" />{ todo.author_data.name }</span>
+                <div className="TodoRightWrapper">
+                    {this.renderRightWrapper(this.props.todo)}
                 </div>
-            </div>
-            <div className="TodoRightWrapper">
-                <Button positive circular icon="checkmark" />
-                <Button circular icon="trash" />
-            </div>
-        </li>
-    )
+            </li>
+        )
+    }
 };
 
 export default TodoList;
