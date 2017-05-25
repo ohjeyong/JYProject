7d1945446ac59c2209bac8a8c38c94102d2fcfab
ohjeyong <ojy6042@kaist.ac.kr>
2017-05-25 22:11:08 +0900

    remove login permission on tag view

--- JYProject/api/todo_view.py
+++ JYProject/api/todo_view.py
@@ -59,4 +59,3 @@ class TodoViewSet(viewsets.ModelViewSet):
 class TagListView(generics.ListAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-    permission_classes = [IsAuthenticated]
