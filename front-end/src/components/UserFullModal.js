f05a92c618a0c0802e36bf3cc449059e8eee3e7b
ohjeyong <ojy6042@kaist.ac.kr>
2017-05-27 16:20:04 +0900

    login and signup first commit

--- front-end/src/components/UserFullModal.js
+++ front-end/src/components/UserFullModal.js
@@ -2,10 +2,22 @@
  * Created by oh on 5/26/17.
  */
 import React, { Component } from 'react';
-import { Dimmer, Icon, Button } from 'semantic-ui-react';
+import { Dimmer, Icon, Button, Form } from 'semantic-ui-react';
 
 
 class UserFullModal extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            showLoginForm: true, // if true, then show login form, else, show signup form
+            loginId: '',
+            loginPassword: '',
+            signupId: '',
+            signupName: '',
+            signupPassword: '',
+            signupPassword2: ''
+        }
+    }
 
     componentDidMount(){
         this.props.fetchUserInfo();
@@ -23,10 +35,80 @@ class UserFullModal extends Component {
         this.props.handleClose();
     };
 
+    handleLoginSignupChange = (e, { name, value }) => this.setState({[name]: value});
+
+    handleClickLoginButton = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    };
+
+    handleClickSignupButton = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    };
+
+    renderLoginSignupForm = () => {
+        if(this.state.showLoginForm){
+            return (
+                <Form inverted>
+                    <Form.Group>
+                        <Form.Input label="ID" placeholder="아이디를 입력해주세요." width={16}
+                                    onChange={this.handleLoginSignupChange}
+                                    value={this.state.loginId}
+                                    name="loginId"
+                        />
+                        <Form.Input label="PASSWORD" placeholder="비밀번호를 입력해주세요." width={16} type="password"
+                                    onChange={this.handleLoginSignupChange}
+                                    value={this.state.loginPassword}
+                                    name="loginPassword"
+                        />
+                    </Form.Group>
+                    <Button onClick={(e) => this.handleClickLoginButton(e)} type="submit" primary>로그인</Button>
+                </Form>
+            )
+        }else{
+            return (
+                <Form inverted>
+                    <Form.Group>
+                        <Form.Input label="아이디" placeholder="아이디를 입력해주세요." width={16}
+                                    onChange={this.handleLoginSignupChange}
+                                    value={this.state.signupId}
+                                    name="signupId"
+                        />
+                        <Form.Input label="이름" placeholder="이름을 입력해주세요." width={16}
+                                    onChange={this.handleLoginSignupChange}
+                                    value={this.state.signupName}
+                                    name="signupName"
+                        />
+                        <Form.Input label="비밀번호" placeholder="비밀번호를 입력해주세요." width={16} type="password"
+                                    onChange={this.handleLoginSignupChange}
+                                    value={this.state.signupPassword}
+                                    name="signupPassword"
+                        />
+                        <Form.Input label="비밀번호 재확인" placeholder="비밀번호를 한 번 더 입력해주세요." width={16} type="password"
+                                    onChange={this.handleLoginSignupChange}
+                                    value={this.state.signupPassword2}
+                                    name="signupPassword2"
+                        />
+                    </Form.Group>
+                    <Button onClick={(e) => this.handleClickSignupButton(e)} type="submit" primary>회원가입</Button>
+                </Form>
+            )
+        }
+    };
+
     renderContent = () => {
         if(typeof(this.props.user.id) === 'undefined'){
             return (
-                <span>WHO ARe YOU</span>
+                <div>
+                    <div>
+                        <div onClick={() => this.setState({ showLoginForm: true })}>Login</div>
+                        <div onClick={() => this.setState({ showLoginForm: false })}>SignUp</div>
+                    </div>
+                    <div>
+                        { this.renderLoginSignupForm() }
+                    </div>
+                </div>
             )
         }else{
             return (
