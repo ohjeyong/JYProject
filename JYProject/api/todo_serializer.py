8d096bc8484c023e10c9deaa3ae9b41a55a19273
ohjeyong <ojy6042@kaist.ac.kr>
2017-05-29 12:32:36 +0900

    add todocomment serializer

--- JYProject/api/todo_serializer.py
+++ JYProject/api/todo_serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from JYProject.todo.models import Todo, Tag
+from JYProject.todo.models import Todo, Tag, TodoComment
 from .user_serializer import UserSerializer
 
 
@@ -8,11 +8,12 @@ class TodoSerializer(serializers.ModelSerializer):
 
     author_data = serializers.SerializerMethodField(read_only=True)
     tag_list = serializers.SerializerMethodField(read_only=True)
+    todo_comment_list = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Todo
         fields = ('id', 'content', 'like', 'category', 'is_completed', 'complete_at', 'author', 'author_data',
-                  'created_at', 'last_edit_at', 'tag_list')
+                  'created_at', 'last_edit_at', 'tag_list', 'todo_comment_list')
 
     def get_author_data(self, obj):
         return UserSerializer(obj.author, read_only=True).data
@@ -20,8 +21,22 @@ class TodoSerializer(serializers.ModelSerializer):
     def get_tag_list(self, obj):
         return TagSerializer(obj.tag.all(), many=True).data
 
+    def get_todo_comment_list(self, obj):
+        return TodoCommentSerializer(obj.todocomment_set.all(), many=True).data
+
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ('id', 'name')
+
+
+class TodoCommentSerializer(serializers.ModelSerializer):
+    author_data = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = TodoComment
+        fields = ('id', 'author', 'author_data', 'content', 'created_at')
+
+    def get_author_data(self, obj):
+        return UserSerializer(obj.author, read_only=True).data
