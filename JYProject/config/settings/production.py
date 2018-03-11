81da79720984c21957521822816738b7f92b3c24
ohjeyong <ojy6042@gmail.com>
2018-03-11 04:38:19 -0700

    remove database settings

--- JYProject/config/settings/production.py
+++ JYProject/config/settings/production.py
@@ -4,17 +4,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['52.78.65.32', 'jylist.cc']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': get_env_variables('DATABASE_NAME'),
-        'USER': get_env_variables('DATABASE_USER'),
-        'PASSWORD': get_env_variables('DATABASE_PASSWORD'),
-        'HOST': get_env_variables('DATABASE_HOST'),
-        'PORT': "5432"
-    }
-}
-
 STATICFILES_DIRS += [
     os.path.join(REACT_APP_DIR, 'build', 'static')
 ]
